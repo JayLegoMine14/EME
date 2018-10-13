3bbfc27834c6a2ca925fbf6f43af2b94a5c5b79d
Jacob Russell <jacobhenryrussell@gmail.com>
2018-10-13 01:48:24 -0500

    Added sentiment analysis
    Accepts plain text and returns a sentiment object with score and magnitude. Addtional toString method as well.

--- EME/Program.cs
+++ EME/Program.cs
@@ -9,14 +9,23 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
+
 namespace EME
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            // James' stuff
             //List<string> urls = BingService.GetURLs("Thomas Aquines");
-            BuildWebHost(args).Run();
+            //BuildWebHost(args).Run();
+
+            var someText = "Donald Trump sucks eggs.";
+            Google.Cloud.Language.V1.Sentiment sentiment = NaturalLanguageAnalysis.getSentiment(someText);
+            System.Diagnostics.Debug.WriteLine("Sentiment: " + sentiment.Score);
+            System.Diagnostics.Debug.WriteLine("Magnitude: " + sentiment.Magnitude);
+            System.Diagnostics.Debug.WriteLine(NaturalLanguageAnalysis.sentimentToString(sentiment));
+
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
