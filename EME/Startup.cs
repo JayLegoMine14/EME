3f27a46df3fd44ee2a49fbeb96169e26eba38a8d
James Allison <jaylegomanallison@gmail.com>
2018-10-12 21:38:22 -0500

    Added Bing Search

--- EME/Startup.cs
+++ EME/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EME.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -14,6 +15,7 @@ namespace EME
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+         
         }
 
         public IConfiguration Configuration { get; }
